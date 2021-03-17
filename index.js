8ea13fa05a47f7315cde870f5031a9e97d4ac119
Oleksandr Gorpynich <oleksandr.gorpynich@gmail.com>
2021-03-17 08:20:36 -0700

    Routes create their own db managers

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require('express');
-const { initDB, initManagers } = require('./server/db/init_db.js');
-const routes = require('./server/routes.js');
+const { initDB } = require('./server/db/init_db.js');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const app = express();
 
 initDB().then(() => {
-	const { postManager, userManager } = initManagers();
-	userManager.testAddUser();
-
+	// eslint-disable-next-line node/global-require
+	const routes = require('./server/routes.js');
 	app.use(bodyParser.json());
-	routes(app, postManager, userManager);
+	routes(app);
 
 	app.listen(process.env.PORT, () => console.log('listening on %d', process.env.PORT));
 }).catch(error => {
--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,7 @@
-module.exports = function (app, postManager, userManager) {
+const { initManagers } = require('./db/init_db.js');
+// eslint-disable-next-line no-unused-vars
+const { postManager, userManager } = initManagers();
+module.exports = function (app) {
 	app.post('/posts/add', function (req, res) {
 		console.log(req.body);
 		res.sendStatus(200);
