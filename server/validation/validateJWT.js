f592df5db89e60eeb8ac825bfaa3a86ed4b5db7e
Oleksandr Gorpynich <oleksandr.gorpynich@gmail.com>
2021-03-30 17:35:04 -0700

    removed uneccesary nesting

--- server/validation/validateJWT.js
+++ server/validation/validateJWT.js
@@ -1,17 +1,18 @@
 const jwt = require('jsonwebtoken');
 module.exports = function (req, res, next) {
 	const authHeader = req.headers.authorization;
-	if (authHeader) {
-		const token = authHeader.split(' ')[1];
-		try {
-			const result = jwt.verify(token, process.env.JWT_SECRET);
-			if (result.username === req.cookies.username) {
-				next();
-				return;
-			}
-		} catch (e) {
-			return res.sendStatus(401);
+	if (!authHeader) {
+		return res.sendStatus(401);
+	}
+	const token = authHeader.split(' ')[1];
+	try {
+		const result = jwt.verify(token, process.env.JWT_SECRET);
+		if (result.username === req.cookies.username) {
+			next();
+			return;
 		}
+	} catch (e) {
+		return res.sendStatus(401);
 	}
 	return res.sendStatus(401);
 };
