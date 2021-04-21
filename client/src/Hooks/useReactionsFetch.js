7c2d97eef8d96bc006b61d4b5eb96116d1499239
Oleksandr Gorpynich <oleksandr.gorpynich@gmail.com>
2021-04-21 11:17:32 -0700

    Return error in hooks

--- client/src/Hooks/useReactionsFetch.js
+++ client/src/Hooks/useReactionsFetch.js
@@ -2,14 +2,17 @@ import { useState, useEffect } from 'react';
 import api from '../api.js';
 export default function useReactionsFetch () {
 	const [reactions, setReactions] = useState({});
-
+	const [error, setError] = useState();
 	useEffect(() => {
 		api.sendReactionsRequest().then((res) => {
 			setReactions(res.data);
-		}).catch((error) =>
-			console.log(error)
-		);
+		}).catch((error) => {
+			setError(error);
+		});
 	}, []);
 
+	if (error) {
+		return error;
+	}
 	return reactions;
 }
