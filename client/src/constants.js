94af1f167a69bc6da0409db5402e9d565f50c7c8
Oleksandr Gorpynich <oleksandr.gorpynich@gmail.com>
2021-06-28 09:42:20 -0700

    Check URI type + use milliseconds for expiration time

--- client/src/constants.js
+++ client/src/constants.js
@@ -2,7 +2,7 @@
 const POSTS_PER_PAGE = 20;
 // REFRESH_TOKEN_TIME: how often a request gets sent to the server to create a new access token.
 // Make sure this number is less the the token expiration time in .env
-const REFRESH_TOKEN_TIME = 600000;
+const REFRESH_TOKEN_TIME = 500 * 1000;
 // MAX_COMMENT_DEPTH: the depth to which comments are displayed.
 // Should be same as MAX_COMMENT_DEPTH in .env
 const MAX_COMMENT_DEPTH = 3;
--- server/db/initDB.ts
+++ server/db/initDB.ts
@@ -7,6 +7,9 @@ require('dotenv').config();
 
 export async function initDB() {
 	try {
+		if (typeof process.env.URI !== 'string') {
+			throw new Error('URI not set in .env');
+		}
 		await mongoose.connect(process.env.URI as string);
 	} catch (e) {
 		console.log('Cannot connect to database');
