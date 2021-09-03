06b505ed34b45338faac882cf1df4b3a14554d31
Oleksandr Gorpynich <oleksandr.gorpynich@gmail.com>
2021-09-03 16:10:21 -0700

    Added most routes to swagger

--- server/routes/posts/index.ts
+++ server/routes/posts/index.ts
@@ -4,24 +4,49 @@ import postSchema from '../../schemas/postSchema';
 import postRequestSchema from '../../schemas/postRequestSchema';
 import postController from '../../controllers/postController';
 import wrap from '../../controllers/wrap';
+import validateRefreshJWT from '../../validation/validateRefreshJWT';
 import express = require('express');
 const app = express();
 
 // submit a post
-app.post('/api/v1/posts', validateAccessJWT, celebrate(postSchema), wrap(postController.postPosts));
+app.post(
+	'/api/v1/posts',
+	validateAccessJWT,
+	celebrate(postSchema),
+	wrap(postController.postPosts)
+);
 
 // retrieve posts
-app.get('/api/v1/posts', celebrate(postRequestSchema), wrap(postController.getPosts));
+app.get(
+	'/api/v1/posts',
+	celebrate(postRequestSchema),
+	wrap(postController.getPosts)
+);
 
 // retrieve a post based on id
 app.get('/api/v1/posts/:id', wrap(postController.getPostsId));
 
 app.get('/api/v1/posts-number', wrap(postController.getPostsNumber));
 
-app.post('/api/v1/posts/:id/upvote', validateAccessJWT, wrap(postController.upvotePost));
+app.post(
+	'/api/v1/posts/:id/upvote',
+	validateAccessJWT,
+	validateRefreshJWT,
+	wrap(postController.upvotePost)
+);
 
-app.post('/api/v1/posts/:id/downvote', validateAccessJWT, wrap(postController.downvotePost));
+app.post(
+	'/api/v1/posts/:id/downvote',
+	validateAccessJWT,
+	validateRefreshJWT,
+	wrap(postController.downvotePost)
+);
 
-app.post('/api/v1/posts/:id/remove-reactions', validateAccessJWT, wrap(postController.removePostReactions));
+app.post(
+	'/api/v1/posts/:id/remove-reactions',
+	validateAccessJWT,
+	validateRefreshJWT,
+	wrap(postController.removePostReactions)
+);
 
 module.exports = app;
